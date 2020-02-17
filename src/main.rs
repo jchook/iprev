32ea42207fc3870dd8e1f4e286aef940f59bfad1
Wes Roberts <g0r6p@a.zinc.email>
2020-02-17 01:35:10 -0500

    Removes suffixes

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,26 @@ use std::process;
 
 fn ip_reverse(ip: &IpAddr) -> String {
     match ip {
-        IpAddr::V4(ip4) => ip4_reverse(ip4, "in-addr.arpa"),
-        IpAddr::V6(ip6) => ip6_reverse(ip6, "ip6.arpa"),
+        IpAddr::V4(ip4) => ip4_reverse(ip4),
+        IpAddr::V6(ip6) => ip6_reverse(ip6),
     }
 }
 
-fn ip4_reverse(ip: &Ipv4Addr, suffix: &str) -> String {
-    let mut rev = String::with_capacity(16 + suffix.len());
+fn ip4_reverse(ip: &Ipv4Addr) -> String {
+    let mut rev = String::with_capacity(16);
     for oct in ip.octets().iter().rev() {
         rev.push_str(format!("{}.", oct).as_str());
     }
-    rev.push_str(suffix);
     rev
 }
 
-fn ip6_reverse(ip: &Ipv6Addr, suffix: &str) -> String {
-    let mut rev = String::with_capacity(64 + suffix.len());
+fn ip6_reverse(ip: &Ipv6Addr) -> String {
+    let mut rev = String::with_capacity(64);
     for oct in ip.octets().iter().rev() {
         rev.push_str(format!("{:x}.", oct & 0x0fu8).as_str());
         // Rust gets mad about right-shifting set bits, so we need to mask + shift
         rev.push_str(format!("{:x}.", (oct & 0xf0u8) >> 4u8).as_str());
     }
-    rev.push_str(suffix);
     rev
 }
 
